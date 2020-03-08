e6fa6024cf99fe425649cb5a5474c73e92301ee1
Harley Garcia <hagarcia99@outlook.com>
2020-03-08 15:20:55 -0600

    menu, footer, and bootstrap

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor (
+    public id:number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +19,12 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    { id: 1, description: 'Work Out' },
-    { id: 2, description: 'Play Games'},
-    { id: 3, description: 'Code'}
+    new Todo (1, 'Work Out', false, new Date()),
+    new Todo (2, 'Code', false, new Date()),
+    new Todo (3, 'Play Games', false, new Date())
+    // { id: 1, description: 'Work Out' },
+    // { id: 2, description: 'Play Games'},
+    // { id: 3, description: 'Code'}
   ]
 
   constructor() { }
