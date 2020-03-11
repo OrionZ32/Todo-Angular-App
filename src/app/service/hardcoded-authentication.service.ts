42ae7b31260e5b59ab66f89c8437b12072c40b49
Harley Garcia <hagarcia99@outlook.com>
2020-03-10 19:59:58 -0600

    session storage

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -8,9 +8,17 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password) {
+    console.log('before ' + this.isUserLoggedIn());
     if(username === 'Orion' && password === 'pass') {
+      sessionStorage.setItem('authenticateUser', username);
+      console.log('after ' + this.isUserLoggedIn());
       return true;
     }
     return false;
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticateUser');
+    return !(user == null)
   }
 }
