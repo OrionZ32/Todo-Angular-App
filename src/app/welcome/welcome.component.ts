b69d00170145f9653f02e3fba3ca5f52c6599cd4
Harley Garcia <hagarcia99@outlook.com>
2020-03-16 09:37:54 -0600

    beginning setup for data service

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -4,6 +4,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -15,12 +16,19 @@ export class WelcomeComponent implements OnInit {
 
   name:string = ''
 
-  constructor(private route:ActivatedRoute) {
+  constructor(
+    private route:ActivatedRoute,
+    private service:WelcomeDataService
+    ) {
 
-  }
+    }
 
   ngOnInit() {
     this.name = this.route.snapshot.params['name'];
+  }
+
+  getWelcomeMessage() {
+    this.service.executeHelloWorldBeanService();
   }
 
 }
